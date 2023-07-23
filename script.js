ff82fe88f658b8828835034ea65cfe655a52a846
Zhen <zishenchan@gmail.com>
2023-07-23 21:58:56 +0800

    finalised

---
+++ script.js
@@ -0,0 +1,9 @@
+function hideMenu(){
+    const menu = document.getElementById("menu");
+    menu.style.right = "-300px";
+}
+
+function showMenu(){
+    const menu = document.getElementById("menu");
+    menu.style.right = "0";
+}
